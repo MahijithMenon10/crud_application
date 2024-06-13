b15dcbb82206ec9999bed0bffea714468c630256
Mahijith <menon.mahijith@gmail.com>
2024-06-13 17:09:48 +0530

    adding cors

--- server/index.js
+++ server/index.js
@@ -9,8 +9,30 @@ dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors({ origin: true }));
-cors({ credentials: true, origin: true });
+
+const allowedOrigins = [
+  'https://crud-application-psi.vercel.app/',
+  'http://localhost:5173',
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          'The CORS policy for this site does not ' +
+          'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
+
 app.use(bodyParser.urlencoded({ extended: true }));
 connectDB();
 app.get('/', (req, res) => {
